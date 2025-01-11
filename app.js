e271cd4f9cd3caa9c56203e0f9fc4625389917c5
FarhhanSyed <farhansyed2045@gmail.com>
2025-01-11 17:50:35 +0530

    basic setup

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
 const express=require("express");
+const mongoose=require("mongoose");
 const app=express();
+const path=require("path");
+
+app.set("view engine","ejs");
+app.use(express.static(path.join(__dirname,"/view")));
+app.use(express.static(path.join(__dirname,"/public")));
+app.use(express.urlencoded({extended:true}));
+
+async function main(){
+    await mongoose.connect("mongodb://127.0.0.1:27017/QuickDocs");
+}
+
+main()
+.then(()=>{
+    console.log("Connection Successfull");
+})
+.catch((err)=>{
+    console.log(err);
+})
+
+app.get("/quickDocs/login",(req,res)=>{
+    res.render("login.ejs");
+})
 
 app.get("/",(req,res)=>{
     res.send("root working");
