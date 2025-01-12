8b9edf69988b901d6a929e6dc63d00b1a5a1a1ce
FarhhanSyed <farhansyed2045@gmail.com>
2025-01-12 12:03:53 +0530

    home page

--- app.js
+++ app.js
@@ -2,11 +2,17 @@ const express=require("express");
 const mongoose=require("mongoose");
 const app=express();
 const path=require("path");
+const expressLayouts = require("express-ejs-layouts");
+const { title } = require("process");
+
 
 app.set("view engine","ejs");
+app.set("layout", "layouts/boilerplate"); 
 app.use(express.static(path.join(__dirname,"/view")));
 app.use(express.static(path.join(__dirname,"/public")));
 app.use(express.urlencoded({extended:true}));
+app.use(expressLayouts);
+
 
 async function main(){
     await mongoose.connect("mongodb://127.0.0.1:27017/QuickDocs");
@@ -20,8 +26,16 @@ main()
     console.log(err);
 })
 
+app.get("/quickDocs/home",(req,res)=>{
+    res.render("pages/home.ejs",{title:"home",stylesheet:"home.css"});
+})
+
 app.get("/quickDocs/login",(req,res)=>{
-    res.render("login.ejs");
+    res.render("pages/login.ejs",{title:"login",stylesheet:"login.css"});
+})
+
+app.get("/quickDocs/onboardingA",(req,res)=>{
+    res.render("/pages/onboardingA.ejs",{title:"onboardingA",stylesheet:"onboarding.css"});
 })
 
 app.get("/",(req,res)=>{
