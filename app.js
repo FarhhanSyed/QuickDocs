c3a8b7bd9f32976f9bd3009dcff9ceccbb1d5c51
vasudevshetty <vasudeepu2815@gmail.com>
2025-01-25 00:32:32 +0530

    reworking on the project, shoud finsish the backend to demostate the stff.

--- app.js
+++ app.js
@@ -1,71 +1,26 @@
-const express=require("express");
-const mongoose=require("mongoose");
-const app=express();
-const path=require("path");
-const expressLayouts = require("express-ejs-layouts");
-const { title } = require("process");
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
+dotenv.config();
 
-app.set("view engine","ejs");
-app.set("layout", "layouts/boilerplate"); 
-app.use(express.static(path.join(__dirname,"/views")));
-app.use(express.static(path.join(__dirname,"/public")));
-app.use(express.urlencoded({extended:true}));
-app.use(expressLayouts);
+const app = express();
 
-
-async function main(){
-    await mongoose.connect("mongodb://127.0.0.1:27017/QuickDocs");
+async function connectDB() {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {});
+    console.log("Connection Successful");
+  } catch (err) {
+    console.error("Connection error", err);
+  }
 }
 
-main()
-.then(()=>{
-    console.log("Connection Successfull");
-})
-.catch((err)=>{
-    console.log(err);
-})
-
-app.get("/quickDocs",(req,res)=>{
-    res.render("pages/home.ejs",{title:"home",stylesheet:"home.css"});
-})
-
-app.get("/quickDocs/onboardingA",(req,res)=>{
-    res.render("pages/onboardingA.ejs",{title:"onboardingA",stylesheet:"onboardingA.css"});
-})
-
-app.get("/quickDocs/onboardingB",(req,res)=>{
-    res.render("pages/onboardingB.ejs",{title:"onboardingB",stylesheet:"onboardingB.css"});
-})
-
-app.get("/quickDocs/onboardingC",(req,res)=>{
-    res.render("pages/onboardingC.ejs",{title:"onboardingc",stylesheet:"onboardingC.css"});
+app.get("/", (req, res) => {
+  res.send("root working");
 });
 
-app.get("/quickDocs/login",(req,res)=>{
-    res.render("pages/login.ejs",{title:"login",stylesheet:"login.css"});
-})
-
-app.get("/quickDocs/upload",(req,res)=>{
-    res.render("pages/upload.ejs",{title:"upload",stylesheet:"upload.css"});
-})
-
-app.get("/quickDocs/employeeLogin",(req,res)=>{
-    res.render("pages/employeeLogin.ejs",{title:"employeeLogin",stylesheet:"employeeLogin.css"});
-})
-
-app.get("/quickDocs/verification",(req,res)=>{
-    res.render("pages/verification.ejs",{title:"verification",stylesheet:"verification.css"});
-})
-
-app.get("/quickDocs/verificationComplete",(req,res)=>{
-    res.render("pages/verificationComplete.ejs",{title:"complete",stylesheet:"verificationComplete.css"});
-})
-
-app.get("/",(req,res)=>{
-    res.send("root working");
-})
-
-app.listen(8080,()=>{
-    console.log(`Server started on port 8080`);
-})
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server started on port ${PORT}`);
+});
