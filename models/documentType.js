34806e40f0c8cffc7a2e3aefde5b04e56b90dbe6
vasudevshetty <vasudeepu2815@gmail.com>
2025-01-28 11:43:22 +0530

    restartred.

--- models/documentType.js
+++
@@ -1,6 +0,0 @@
-const documentTypeSchema = new Schema({
-  typeName: { type: String, required: true, unique: true }, // e.g., "Official", "Personal"
-  description: { type: String }, // Optional: Description of the type
-});
-
-module.exports = mongoose.model("DocumentType", documentTypeSchema);
